468c8a2f52744c49e4b123c546d2597a43abc3dd
Layne Granados Mogollon <laynegranadosmogollon@MacBook-Pro-de-Layne.local>
2015-08-16 21:09:19 -0500

    Updating gitignore file

--- Android/template v1.1 studio/app/src/main/java/com/special/utils/ConstantsUtils.java
+++ Android/template v1.1 studio/app/src/main/java/com/special/utils/ConstantsUtils.java
@@ -5,8 +5,8 @@ public class ConstantsUtils {
 	public static final String URL_ROOT_TWITTER_API = "https://api.twitter.com";
 	public static final String URL_SEARCH = URL_ROOT_TWITTER_API + "/1.1/search/tweets.json?q=";
 	public static final String URL_AUTHENTICATION = URL_ROOT_TWITTER_API + "/oauth2/token";
-	//public static final String URL_GENERIC= "http://192.168.0.17:8080/LugaresVP/rest/";
-	public static final String URL_GENERIC= "http://172.20.10.7:8080/LugaresVP/rest/";
+	public static final String URL_GENERIC= "http://192.168.0.11:8080/LugaresVP/rest/";
+	//public static final String URL_GENERIC= "http://172.20.10.7:8080/LugaresVP/rest/";
     public static final String URL_LOGIN= URL_GENERIC+"login/inicio";
     public static final String URL_SUPERVISIONDELUGAR= URL_GENERIC+"supervision/lugar";
     public static final String URL_LUGARPORID= URL_GENERIC+"lugar/identificador";
